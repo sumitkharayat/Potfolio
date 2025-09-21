57878e832a9f67673443a4c8acdbacc748dff80e
Sumit Kharayat <sumitkharayat@gmail.com>
2025-09-21 23:00:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+// Contact Form Alert
+document.getElementById("contactForm").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("Thank you for reaching out! I will get back to you soon.");
+  this.reset();
+});
